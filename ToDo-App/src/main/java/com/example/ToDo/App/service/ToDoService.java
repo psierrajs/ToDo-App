9f78c15526c4bd97220306de2422a069e45d4d48
psierrajs <psierrajs@gmail.com>
2024-08-07 09:12:08 +0200

    Added code for getToDoItemById method:   - Retrieves a single to-do item from the repository by its ID   - Uses findById to fetch the item and returns it directly

--- ToDo-App/src/main/java/com/example/ToDo/App/service/ToDoService.java
+++ ToDo-App/src/main/java/com/example/ToDo/App/service/ToDoService.java
@@ -1,16 +1,28 @@
-package com.example.ToDo.App;
+package com.example.ToDo.App.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.ToDo.App.ToDo;
+import com.example.ToDo.App.repo.IToDoRepo;
 
 @Service
 public class ToDoService {
 	
-	List<ToDo> getAllToDoItems(){
+	@Autowired
+	IToDoRepo repo;
+	public List<ToDo> getAllToDoItems(){
+		ArrayList<ToDo> todoList = new ArrayList<>();
+		repo.findAll().forEach(todo -> todoList.add(todo));
 		
+		return todoList;
 	}
 	
-	ToDo getToDoItemById(){
-		
+	public ToDo getToDoItemById(Long id){
+		return repo.findById(id).get();
 		
 	}
 	
